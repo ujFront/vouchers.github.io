44266f2da0ab9cc178e7366f063ae89d6102b244
Jiri Stastny <jiri@urbanjungleirc.com>
2025-07-29 15:21:51 +0800

    new depl and default picture

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const CONFIG = {
     // Google Apps Script Web App URL
     // Replace YOUR_SCRIPT_ID with your actual Google Apps Script ID
-    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbzAWoPcABuRpiBj1lGrDG4wwH6BnMg_kebG8qovPQWZLceiunJGWr9azUQ-Kn7_A8vF/exec',
+    API_BASE_URL: 'https://script.google.com/a/macros/urbanjungleirc.com/s/AKfycbwqc2i63xHvOyvCdoZuJGNfSCxk9SPmo5khUEGj8WoLL9EdwKfbzpdXs2KZn0sSs8s/exec',
     
     // PayPal Configuration
     // For sandbox testing, use sandbox client ID and environment
